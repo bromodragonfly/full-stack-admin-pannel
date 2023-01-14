97091eb9d1f11d25eeb1e5c72573f0c401666fd4
bromodragonfly <87935674+bromodragonfly@users.noreply.github.com>
2023-01-14 20:00:19 +0300

    initial commit

---
+++ server/index.js
@@ -0,0 +1,45 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import bodyParser from 'body-parser'
+import dotenv from 'dotenv'
+import helmet from 'helmet'
+import morgan from 'morgan'
+import clientRoutes from './routes/client.js'
+import generalRoutes from './routes/general.js'
+import managmentRotes from './routes/managment.js'
+import salesRoutes from './routes/sales.js'
+
+// configuration
+dotenv.config()
+const app = express()
+app.use(express.json())
+app.use(helmet())
+app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }))
+app.use(morgan('common'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cors())
+mongoose.set('strictQuery', true)
+
+//routes
+app.use('/clients', clientRoutes)
+app.use('/general', generalRoutes)
+app.use('/managment', managmentRotes)
+app.use('/sales', salesRoutes)
+
+//mongoose SETUP
+
+const PORT = process.env.PORT || 9001
+mongoose
+    .connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server started on ${PORT}`)
+        })
+    })
+    .catch((error) => console.log(`Not connected`, error))
+console.log(process.env.PORT)
