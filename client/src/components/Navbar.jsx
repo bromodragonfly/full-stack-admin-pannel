9e530b39422aeaba6b8c59a52e6748990ed07556
bromodragonfly <87935674+bromodragonfly@users.noreply.github.com>
2023-01-31 19:52:10 +0300

    navbar ready

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,6 +6,7 @@ import {
     SettingsOutlined,
     ArrowDropDownCircleOutlined,
     Search,
+    ArrowDropDownOutlined,
 } from '@mui/icons-material'
 import FlexBetween from './Flexbetween'
 import { useDispatch } from 'react-redux'
@@ -14,15 +15,25 @@ import profileImage from 'assets/profile.jpeg'
 import {
     AppBar,
     Avatar,
+    Button,
     IconButton,
     InputBase,
     Toolbar,
     useTheme,
+    Box,
+    Typography,
+    Menu,
+    MenuItem,
 } from '@mui/material'
 
-const Navbar = ({ isSidebarOpen, setIsSidebarOpen }) => {
+const Navbar = ({ user, isSidebarOpen, setIsSidebarOpen }) => {
     const dispatch = useDispatch()
     const theme = useTheme()
+
+    const [anchorEl, setAnchorEl] = useState(null)
+    const isOpen = Boolean(anchorEl)
+    const handleClick = (event) => setAnchorEl(event.currentTarget)
+    const handleClose = () => setAnchorEl(null)
 
     return (
         <AppBar
@@ -68,9 +79,66 @@ const Navbar = ({ isSidebarOpen, setIsSidebarOpen }) => {
                     <IconButton>
                         <SettingsOutlined sx={{ fontSize: '25px' }} />
                     </IconButton>
-                    <IconButton>
-                        <Avatar src={profileImage} />
-                    </IconButton>
+                    <FlexBetween>
+                        <Button
+                            onClick={handleClick}
+                            sx={{
+                                display: 'flex',
+                                justifyContent: 'space-between',
+                                alignItems: 'center',
+                                textTransform: 'none',
+                                gap: '1rem',
+                            }}
+                        >
+                            <Box
+                                component="img"
+                                alt="profile"
+                                src={profileImage}
+                                height="40px"
+                                width="40px"
+                                borderRadius="50%"
+                                sx={{ objectFit: 'cover' }}
+                            />
+                            <Box textAlign="left">
+                                <Typography
+                                    fontWeight="bold"
+                                    fontSize="0.9rem"
+                                    sx={{
+                                        color: theme.palette.secondary[100],
+                                    }}
+                                >
+                                    {user.name ? user.name : 'Name'}
+                                </Typography>
+                                <Typography
+                                    fontSize="0.8rem"
+                                    sx={{
+                                        color: theme.palette.secondary[200],
+                                    }}
+                                >
+                                    {user.occupation
+                                        ? user.occupation
+                                        : 'Occupation'}
+                                </Typography>
+                            </Box>
+                            <ArrowDropDownOutlined
+                                sx={{
+                                    color: theme.palette.secondary[300],
+                                    fontSize: '25px',
+                                }}
+                            />
+                        </Button>
+                        <Menu
+                            anchorEl={anchorEl}
+                            open={isOpen}
+                            onClose={handleClose}
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'center',
+                            }}
+                        >
+                            <MenuItem onClick={handleClose}>Log Out</MenuItem>
+                        </Menu>
+                    </FlexBetween>
                 </FlexBetween>
             </Toolbar>
         </AppBar>
